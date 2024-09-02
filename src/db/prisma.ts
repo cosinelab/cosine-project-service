cb8189eff800314a2962fdcd0f51e765fbedb369
Kandit <gun.rodwong@gmail.com>
2024-09-02 20:53:04 +0700

    Fix Prisma Client initialization

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,13 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-prisma
-  .$connect() // Ensures Prisma Client is connected
-  .then(() => {
-    console.log("Prisma Client successfully connected");
-  })
-  .catch((error) => {
-    console.error("Error connecting Prisma Client:", error);
-  });
-
 export default prisma;
